60ea77cfeaede63cd6f454dfc43f37bee1bdfb81
Kirill Makeev <44197850+ikirja@users.noreply.github.com>
2025-03-25 15:34:27 +0300

    new methods: create ai log, get ai logs, get todays ai logs

--- src/lib/log/index.ts
+++ src/lib/log/index.ts
@@ -1,5 +1,7 @@
 import { DB } from '../db';
 
+import { AiLogType } from '../../../types/db';
+
 export async function createLog(text: string): Promise<void> {
   const db = new DB();
 
@@ -10,4 +12,32 @@ export async function getLogs() {
   const db = new DB();
 
   return await db.models.log.find().sort({ createdAt: -1 });
+}
+
+export async function createAiLog(
+  text: string,
+  type: AiLogType,
+): Promise<void> {
+  const db = new DB();
+
+  await db.models.aiLog.create({ text: text, type: type });
+}
+
+export async function getAiLogs() {
+  const db = new DB();
+
+  return await db.models.aiLog.find().sort({ createdAt: -1 });
+}
+
+export async function getTodaysAiLogs() {
+  const db = new DB();
+
+  return await db.models.aiLog
+    .find({
+      createdAt: {
+        $gte: new Date().setHours(0, 0, 0),
+        $lt: new Date().setHours(23, 59, 59),
+      },
+    })
+    .sort({ createdAt: -1 });
 }
