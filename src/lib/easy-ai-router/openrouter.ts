6ad6b630eeaf3c54f299e0dd3c078f408e24282e
Kirill Makeev <44197850+ikirja@users.noreply.github.com>
2025-03-25 15:32:02 +0300

    update chat method, new chat response json parse method, additional types

--- src/lib/easy-ai-router/openrouter.ts
+++ src/lib/easy-ai-router/openrouter.ts
@@ -1,8 +1,10 @@
 import axios from 'axios';
-import { Message } from '../../../types/easy-ai-router';
+
+import { Message, OpenrouterChatResponse } from '../../../types/easy-ai-router';
 
 const HOST = process.env.SERVICE_EASY_AI_ROUTER_HOST || '';
-const ACCESS_OPENROUTER = process.env.SERVICE_EASY_AI_ROUTER_ACCESS_OPENROUTER || '';
+const ACCESS_OPENROUTER =
+  process.env.SERVICE_EASY_AI_ROUTER_ACCESS_OPENROUTER || '';
 const urls = {
   ping: `${HOST}/openrouter/api/v1/ping`,
   key: `${HOST}/openrouter/api/v1/key`,
@@ -25,12 +27,32 @@ export async function key() {
   return response.data;
 }
 
-export async function chat(messages: Message[]) {
+export async function chat(
+  messages: Message[],
+): Promise<OpenrouterChatResponse> {
   const response = await axios(urls.chat, {
     method: 'POST',
     headers: headers,
     data: messages,
   });
 
-  return response.data;
+  return response.data as OpenrouterChatResponse;
+}
+
+export function chatResponseJsonParse(messageContent: string) {
+  const arr = messageContent.split('```');
+
+  let jsonObj = {};
+  arr.forEach((str) => {
+    const codeType = str.slice(0, 4);
+    if (codeType === 'json') {
+      try {
+        jsonObj = JSON.parse(str.slice(4));
+      } catch (error) {
+        return null;
+      }
+    }
+  });
+
+  return jsonObj;
 }
--- types/easy-ai-router.d.ts
+++ types/easy-ai-router.d.ts
@@ -1,10 +1,24 @@
 export type Message = {
   role: Role;
   content: string;
-}
+};
 
-export enum Role {
+export const enum Role {
   System = 'system',
   Assistant = 'assistant',
-  User = 'user'
+  User = 'user',
 }
+
+export const enum OpenrouterLimits {
+  FreeLimit = Number(process.env.SERVICE_EASY_AI_ROUTER_OPENROUTER_LIMIT) ||
+    180,
+}
+
+export type OpenrouterChatResponse = {
+  id: string;
+  choices: Choice[];
+};
+
+export type Choice = {
+  message: Message;
+};
