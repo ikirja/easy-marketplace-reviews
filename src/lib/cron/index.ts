66c331ad91c80fa4671c1fa56afad055134299cd
Kirill Makeev <44197850+ikirja@users.noreply.github.com>
2025-03-25 15:35:17 +0300

    new cron tasks: summarize reviews wildberries, ai summarize reviews wildberries

--- src/lib/cron/index.ts
+++ src/lib/cron/index.ts
@@ -1,13 +1,27 @@
 import cron from 'node-cron';
 import mpReviews from '../mp-reviews';
 import * as task from '../task';
-import { createLog } from '../log';
+import * as log from '../log';
 
-const CRON_TIME_WB_FEEDBACKS = process.env.CRON_WB_FEEDBACKS || '*/1 * * * *';
+import { TaskType } from '../../../types/db';
+import { OpenrouterLimits } from '../../../types/easy-ai-router';
+
+const CRON_TIME_WB_FEEDBACKS =
+  process.env.CRON_TIME_WB_FEEDBACKS || '*/1 * * * *';
+const CRON_TIME_WB_FEEDBACKS_SUMMARIZE =
+  process.env.CRON_TIME_WB_FEEDBACKS_SUMMARIZE || '*/2 * * * *';
+const CRON_TIME_WB_FEEDBACKS_AI_SUMMARIZE =
+  process.env.CRON_TIME_WB_FEEDBACKS_AI_SUMMARIZE || '*/5 * * * *';
 
 const FLAGS = {
   isRunning: {
     mpReviews: {
+      wildberries: false,
+    },
+    summarizeReviews: {
+      wildberries: false,
+    },
+    aiSummarizeReviews: {
       wildberries: false,
     },
   },
@@ -20,7 +34,7 @@ async function updateWBReviews() {
   FLAGS.isRunning.mpReviews.wildberries = true;
 
   try {
-    const uncompletedTask = await task.getUncompletedTask('wbreviews');
+    const uncompletedTask = await task.getUncompletedTask(TaskType.WBReviews);
     if (!uncompletedTask) {
       FLAGS.isRunning.mpReviews.wildberries = false;
       return;
@@ -30,15 +44,85 @@ async function updateWBReviews() {
     await task.setAsCompleted(uncompletedTask._id.toString());
   } catch (error) {
     if (error instanceof Error) {
-      await createLog(error.message);
+      await log.createLog(error.message);
     }
   }
 
   FLAGS.isRunning.mpReviews.wildberries = false;
 }
 
+async function summarizeWBReviews() {
+  if (FLAGS.isRunning.summarizeReviews.wildberries) {
+    return;
+  }
+  FLAGS.isRunning.summarizeReviews.wildberries = true;
+
+  try {
+    const uncompletedTask = await task.getUncompletedTask(
+      TaskType.SummarizeWBReviews,
+    );
+    if (!uncompletedTask) {
+      FLAGS.isRunning.summarizeReviews.wildberries = false;
+      return;
+    }
+
+    const MINIMUM_PRODUCT_VALUATION = 4;
+    await mpReviews.wbReviews.addReviewSumms(MINIMUM_PRODUCT_VALUATION);
+  } catch (error) {
+    if (error instanceof Error) {
+      await log.createLog(error.message);
+    }
+  }
+
+  FLAGS.isRunning.summarizeReviews.wildberries = false;
+}
+
+async function aiSummarizeWBReviews() {
+  if (FLAGS.isRunning.aiSummarizeReviews.wildberries) {
+    return;
+  }
+  FLAGS.isRunning.aiSummarizeReviews.wildberries = true;
+
+  try {
+    const todaysAiLogs = await log.getTodaysAiLogs();
+    if (todaysAiLogs.length >= OpenrouterLimits.FreeLimit) {
+      FLAGS.isRunning.aiSummarizeReviews.wildberries = false;
+      return;
+    }
+
+    const uncompletedTask = await task.getUncompletedTask(
+      TaskType.AISummarizeWBReviews,
+    );
+    if (!uncompletedTask) {
+      FLAGS.isRunning.aiSummarizeReviews.wildberries = false;
+      return;
+    }
+
+    const REQUEST_LIMIT_PER_CRON_TASK = 10;
+    await mpReviews.wbReviews.updateReviewSummsAI(REQUEST_LIMIT_PER_CRON_TASK);
+  } catch (error) {
+    if (error instanceof Error) {
+      await log.createLog(error.message);
+    }
+  }
+
+  FLAGS.isRunning.aiSummarizeReviews.wildberries = false;
+}
+
 export const cronTaskUpdateWBReviews = cron.schedule(
   CRON_TIME_WB_FEEDBACKS,
   updateWBReviews,
+  { scheduled: false },
+);
+
+export const cronTaskSummarizeWBReviews = cron.schedule(
+  CRON_TIME_WB_FEEDBACKS_SUMMARIZE,
+  summarizeWBReviews,
+  { scheduled: false },
+);
+
+export const cronTaskAiSummarizeWBReviews = cron.schedule(
+  CRON_TIME_WB_FEEDBACKS_AI_SUMMARIZE,
+  aiSummarizeWBReviews,
   { scheduled: false },
 );
